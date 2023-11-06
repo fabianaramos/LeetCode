9da905f557797ede447c77092e5ed04789ed6d2d
Fabiana Ramos <fabianavitoriaramos@gmail.com>
2023-11-06 12:00:37 -0300

    feat: improve solution 1470

--- solutions/1470.cpp
+++ solutions/1470.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> x;
-        vector<int> y;
         vector<int> ans;
+        int j = n;
     
-        for (int i = 0; i < nums.size(); i++) {
-            if (i < n) {
-                x.push_back(nums[i]);
-            }
-            else {
-                y.push_back(nums[i]);
-            }
-        }
-        for (int i = 0; i < x.size(); i++) {
-            ans.push_back(x[i]);
-            ans.push_back(y[i]);
+        for (int i = 0; i < n; i++) {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            j++;
         }
     return ans;
     }
